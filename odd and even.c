08312d306929d8da266d1770eb60fff25200b7ba
Dipanshu Mondal <papiyamondaljr@gmail.com>
2026-09-20 20:39:30 +0530

    Add files via upload
    add and even function in c language

---
+++ odd and even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main ()
+{
+    int y;
+    // odd = 0 even = 1
+    printf ("enter your number");
+    scanf("%d", &y);
+    printf("%d", y%2==0 );
+    return 0 ;
+
+
+}
